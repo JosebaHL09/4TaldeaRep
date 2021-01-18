4ddbc8a336e0e650ba2c496e633aeb822224a36a
plopezarco <lopez.pablo@uni.eus>
2021-01-18 10:09:28 +0100

    Aldaketak

---
+++ proiektua/java-spring-rest-real/src/main/java/eus/uni/dam2/java/spring/rest/real/repositories/MongoDBPokemonRepository.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eus.uni.dam2.java.spring.rest.real.repositories;
+
+/**
+ *
+ * @author lopez.pablo
+ */
+public class MongoDBPokemonRepository {
+    
+}
