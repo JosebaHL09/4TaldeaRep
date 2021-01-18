745b54a9bb62c1a8edaba3bc344358f7ae5eb8e0
plopezarco <lopez.pablo@uni.eus>
2021-01-18 10:34:17 +0100

    PokemonController

--- proiektua/java-spring-rest-real/src/main/java/eus/uni/dam2/java/spring/rest/real/controllers/PokemonController.java
+++ proiektua/java-spring-rest-real/src/main/java/eus/uni/dam2/java/spring/rest/real/controllers/PokemonController.java
@@ -4,8 +4,10 @@
  * and open the template in the editor.
  */
 package eus.uni.dam2.java.spring.rest.real.controllers;
+
 import eus.uni.dam2.java.spring.rest.real.model.Pokemon;
 import eus.uni.dam2.java.spring.rest.real.repositories.PokemonRepository;
+import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -19,9 +21,8 @@ import static java.util.Arrays.asList;
 @RestController
 @RequestMapping("/api")
 
-
 public class PokemonController {
-    
+
     private final static Logger LOGGER = LoggerFactory.getLogger(PokemonController.class);
     private final PokemonRepository pokemonRepository;
 
@@ -41,26 +42,31 @@ public class PokemonController {
         return pokemonRepository.saveAll(pokemon);
     }
 
-    @GetMapping("pokemon")
-    public List<Pokemon> getPokemon() {
+    @GetMapping("pokemon/all")
+    public List<Pokemon> getAllPokemon() {
         return pokemonRepository.findAll();
     }
 
     @GetMapping("pokemon/{id}")
-    public ResponseEntity<Pokemon> getPokemones(@PathVariable int id) {
+    public ResponseEntity<Pokemon> getPokemon(@PathVariable int id) {
         Pokemon pokemon = pokemonRepository.findOne(id);
-        if (pokemon == null)
+        if (pokemon == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
         return ResponseEntity.ok(pokemon);
     }
 
-    @GetMapping("pokemones/{ids}")
-    public List<Pokemon> getPokemones(@PathVariable int ids) {
-        List<String> listIds = asList(ids.split(","));
+    @GetMapping("multiplePokemon/{ids}")
+    public List<Pokemon> getMultiplePokemon(@PathVariable String ids) {
+        List<String> listIdsString = asList(ids.split(","));
+        List<Integer> listIds = new ArrayList<>();
+        for (String s : listIdsString) {
+            listIds.add(Integer.valueOf(s));
+        }
         return pokemonRepository.findAll(listIds);
     }
 
-    @GetMapping("pokemones/count")
+    @GetMapping("pokemon/count")
     public Long getCount() {
         return pokemonRepository.count();
     }
@@ -70,14 +76,17 @@ public class PokemonController {
         return pokemonRepository.delete(id);
     }
 
-    @DeleteMapping("pokemones/{ids}")
+    @DeleteMapping("multiplePokemon/{ids}")
     public Long deletePokemones(@PathVariable String ids) {
-        List<String> listIds = asList(ids.split(","));
-       
+        List<String> listIdsString = asList(ids.split(","));
+        List<Integer> listIds = new ArrayList<>();
+        for (String s : listIdsString) {
+            listIds.add(Integer.valueOf(s));
+        }
         return pokemonRepository.delete(listIds);
     }
 
-    @DeleteMapping("pokemones")
+    @DeleteMapping("pokemon/all")
     public Long deletePokemones() {
         return pokemonRepository.deleteAll();
     }
@@ -87,21 +96,16 @@ public class PokemonController {
         return pokemonRepository.update(pokemon);
     }
 
-    @PutMapping("pokemones")
+    @PutMapping("multiplePokemon")
     public Long putPerson(@RequestBody List<Pokemon> pokemon) {
         return pokemonRepository.update(pokemon);
     }
-    /*
-    @GetMapping("pokemones/averageAge")
-    public Double averageAge() {
-        return pokemonRepository.getAverageAge();
-    }
-    */
+
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public final Exception handleAllExceptions(RuntimeException e) {
         LOGGER.error("Internal server error.", e);
         return e;
     }
-    
+
 }
