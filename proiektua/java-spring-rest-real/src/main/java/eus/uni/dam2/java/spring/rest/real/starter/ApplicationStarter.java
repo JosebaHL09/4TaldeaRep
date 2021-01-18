ca5566ab7753492cd2b644f57e1001e7878d1c62
plopezarco <lopez.pablo@uni.eus>
2021-01-18 11:14:33 +0100

    Arreglar todo bien

---
+++ proiektua/java-spring-rest-real/src/main/java/eus/uni/dam2/java/spring/rest/real/starter/ApplicationStarter.java
@@ -0,0 +1,11 @@
+package eus.uni.dam2.java.spring.rest.real.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+}
