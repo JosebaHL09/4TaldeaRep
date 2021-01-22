92d17c743c9c844bfdaf3c445db7448ded988b0d
MarkelSalgado <71440203+MarkelSalgado@users.noreply.github.com>
2021-01-22 09:18:12 +0100

    asp

--- proiektua/ASP-Proiektua/Pokemon_4.Taldea/Pokemon_4.Taldea/Controllers/HomeController.cs
+++ proiektua/ASP-Proiektua/Pokemon_4.Taldea/Pokemon_4.Taldea/Controllers/HomeController.cs
@@ -1,50 +1,17 @@
-﻿using Pokemon_4.Taldea.Models;
-using System;
-using Newtonsoft.Json;
+﻿using System;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
-
 
 namespace Pokemon_4.Taldea.Controllers
 {
     public class HomeController : Controller
     {
-        //Hosted web API REST Service base url  
-        string Baseurl = "http://192.168.72.7:8080/";
-        public async Task<ActionResult> Index()
+        // GET: Home
+        public ActionResult Index()
         {
-            List<Pokemon> PokInfo = new List<Pokemon>();
-
-            using (var client = new HttpClient())
-            {
-                //Passing service base url  
-                client.BaseAddress = new Uri(Baseurl);
-
-                client.DefaultRequestHeaders.Clear();
-                //Define request data format  
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                //Sending request to find web api REST service resource GetAllEmployees using HttpClient 
-              
-                HttpResponseMessage Res = await client.GetAsync("api/pokemon");
-
-                //Checking the response is successful or not which is sent using HttpClient  
-                if (Res.IsSuccessStatusCode)
-                {
-                    //Storing the response details recieved from web api   
-                    var PokResponse = Res.Content.ReadAsStringAsync().Result;
-
-                    //Deserializing the response recieved from web api and storing into the Employee list  
-                    PokInfo = JsonConvert.DeserializeObject<List<Pokemon>>(PokResponse);
-
-                }
-                //returning the employee list to view  
-                return View(PokInfo);
-            }
+            return View();
         }
     }
 }
-   
