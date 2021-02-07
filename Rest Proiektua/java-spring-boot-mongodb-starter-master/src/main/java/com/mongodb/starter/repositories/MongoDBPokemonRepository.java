5e371015f97c3010a3d0f66a8e1daa570f5bc625
plopezarco <lopez.pablo@uni.eus>
2021-02-07 11:41:57 +0100

    ordenar y limpieza full

--- Rest Proiektua/java-spring-boot-mongodb-starter-master/src/main/java/com/mongodb/starter/repositories/MongoDBPokemonRepository.java
+++ Rest Proiektua/java-spring-boot-mongodb-starter-master/src/main/java/com/mongodb/starter/repositories/MongoDBPokemonRepository.java
@@ -155,14 +155,13 @@ public class MongoDBPokemonRepository implements PokemonRepository {
         } finally {
             cursor.close();
         }
-        
         types.sort(new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
                 return s1.compareToIgnoreCase(s2);
             }
         });
-        return types;        
+        return types;
     }
     
     /**
